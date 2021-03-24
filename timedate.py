037f29dab81a7ed335786ffd64306950105a0d07
DXwilliams <h55019@163.com>
2021-03-24 15:16:36 +0800

    Add files via upload

---
+++ timedate.py
@@ -0,0 +1,15 @@
+import time
+
+# def get_time_stamp():
+#     now = int(round(time.time()*1000))
+#     time_stamp = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(now/1000))
+#     print(time_stamp)
+#     stamp = ("".join(time_stamp.split()[0].split("-"))+"".join(time_stamp.split()[1].split(":"))).replace('.', '')
+#     print(stamp)
+
+
+# if __name__ == '__main__':
+#     get_time_stamp()
+
+
+
